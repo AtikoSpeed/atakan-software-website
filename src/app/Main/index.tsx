cafced32093f81454d6c67139b13fc6383c114b5
Atakan Temizkan <atikotemizkan@gmail.com>
2024-11-30 01:10:00 +0300

    removed comments

--- src/app/Main/index.tsx
+++ src/app/Main/index.tsx
@@ -10,9 +10,7 @@ export default function Main() {
         fill={true}
         className="h-screen w-screen -z-10 object-cover brightness-50 contrast-150"
       />
-      {/* Hero */}
       <div className="container h-screen py-24 lg:py-32 flex items-center">
-        {/* Grid */}
         <div className="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
           <div>
             <h2 className="scroll-m-20 text-3xl mb-8">Hello I&apos;m Atakan</h2>
@@ -26,14 +24,11 @@ export default function Main() {
               exceptional user experiences. Let&apos;s bring your vision to life
               on the web!
             </p>
-            {/* Buttons */}
             <div className="mt-12 grid gap-3 w-full sm:inline-flex">
               <Button size={"lg"}>Let&apos;s get started</Button>
             </div>
-            {/* End Buttons */}
             <div className="mt-6 lg:mt-10 grid grid-cols-2 gap-x-5"></div>
           </div>
-          {/* Col */}
           <div className="relative ms-4">
             <Image
               className="w-full rounded-lg"
@@ -43,11 +38,8 @@ export default function Main() {
               height={500}
             />
           </div>
-          {/* End Col */}
         </div>
-        {/* End Grid */}
       </div>
-      {/* End Hero */}
     </main>
   );
 }
