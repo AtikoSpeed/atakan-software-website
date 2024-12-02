c4b926624613805d0cdad746547712d1c96ac407
Atakan Temizkan <atikotemizkan@gmail.com>
2024-12-02 23:41:00 +0300

    fixed issue where nav buttons would be under image and not clickable

--- src/app/Header/index.tsx
+++ src/app/Header/index.tsx
@@ -2,7 +2,7 @@ import Nav from "@/app/Nav";
 
 export default function Header() {
   return (
-    <header className="w-100 flex justify-center">
+    <header className="w-100 flex justify-center z-20">
       <div className="container grid grid-flow-col fixed">
         <div className="flex justify-between items-center py-6">
           <h1 className="text-3xl font-bold glow">Atakan</h1>
