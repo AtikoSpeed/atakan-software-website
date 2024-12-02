14e0c41ade3d88b7235c3d4f21cc13d7b5b80f4d
Atakan Temizkan <atikotemizkan@gmail.com>
2024-12-03 01:03:23 +0300

    added slider and portfolio placeholder

---
+++ src/app/Portfolio/PortfolioSwiper.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+export default function PortfolioSwiper() {
+  return (
+    <div className="h-[60vh] w-[20vw]">
+      <Swiper
+        effect="cards"
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="w-full h-full"
+      >
+        <SwiperSlide className="rounded-lg ">
+          <img
+            src="https://atakan.ninja/images/propic-full.jpg"
+            alt="slide"
+            className="w-full h-full object-cover"
+            href="https://atakan.ninja"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="rounded-lg ">
+          <img
+            src="https://atakan.ninja/images/propic-full.jpg"
+            alt="slide"
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="rounded-lg ">
+          <img
+            src="https://atakan.ninja/images/propic-full.jpg"
+            alt="slide"
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="rounded-lg ">
+          <img
+            src="https://atakan.ninja/images/propic-full.jpg"
+            alt="slide"
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
--- src/app/Portfolio/index.tsx
+++ src/app/Portfolio/index.tsx
@@ -1,10 +1,13 @@
+import PortfolioSwiper from "./PortfolioSwiper";
+
 export default function Portfolio() {
   return (
     <section
       id="portfolio"
-      className="snap-always snap-start container h-screen py-24 lg:py-32 flex items-center justify-center"
+      className="snap-always snap-start container h-screen py-24 lg:py-32 flex flex-col items-center justify-center"
     >
       <h1 className="scroll-m-20 text-3xl mb-8">My Portfolio</h1>
+      <PortfolioSwiper />
     </section>
   );
 }
