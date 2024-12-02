f2fc08f450649ca6a03ac4f6ff22d3894ab212e4
Atakan Temizkan <atikotemizkan@gmail.com>
2024-12-02 23:34:50 +0300

    adjusted navbar padding

--- src/app/Header/index.tsx
+++ src/app/Header/index.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="w-100 flex justify-center">
       <div className="container grid grid-flow-col fixed">
-        <div className="flex justify-between items-center py-4">
+        <div className="flex justify-between items-center py-6">
           <h1 className="text-3xl font-bold glow">Atakan</h1>
         </div>
         <div className="flex justify-end items-center">
